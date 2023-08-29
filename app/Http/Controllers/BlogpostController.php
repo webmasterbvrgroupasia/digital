8eca0fe8e42678e53d83f0779913651786283c28
BvrWebmaster <webmaster1@bvrgroupasia.com>
2023-08-29 17:04:51 +0800

    update blogpostController

--- app/Http/Controllers/BlogpostController.php
+++ app/Http/Controllers/BlogpostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Blogpost;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class BlogpostController extends Controller
